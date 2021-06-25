a3af060dbd999370a8e8810aa78ddcc1b3a46807
daichanghong <daichanghong@jiuqi.com.cn>
2021-06-25 14:22:23 +0800

    项目初始化

---
+++ src/test/java/com/jeooica/eating/EatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeooica.eating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
